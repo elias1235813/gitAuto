87713b87a48787a39f55d91f8b711f518f1ec537
elias1235813 <elias.vuorenmaa706@gmail.com>
2022-12-05 09:06:52 +0200

    lisätty kaikki loput koodit
    lisätty tankkausmetodi, ehto bensanmäärälle, muodostimet ja toinen olio

--- src/gitAuto/main.java
+++ src/gitAuto/main.java
@@ -11,6 +11,11 @@ public class main {
 		auto1.naytaTiedot();
 		auto1.kiihdyta();
 		auto1.naytaTiedot();
+		auto1.tankkaaAuto(5);
+		
+		// muodostetaan toinen auto-olio
+		Auto auto2 = new Auto("Hyundai", "getz", 7);
+		auto2.naytaTiedot();
 
 	}
 
@@ -21,6 +26,18 @@ class Auto{
 	String malli;
 	int bensanMaara = 0;
 	
+	// muodostimet
+	public Auto()
+	{
+			
+	}	
+	public Auto(String merkinNimi, String mallinNimi, int bensaMaara)
+	{
+		merkki = merkinNimi;
+		malli = mallinNimi;
+		bensanMaara = bensaMaara;
+	}
+	
 	//toiminnot
 	public void jarruta()
 	{
@@ -29,13 +46,27 @@ class Auto{
 	public void kiihdyta()
 	{
 		System.out.println("Auto kiihdyttaa");
-		bensanMaara = bensanMaara - 1;
+		if (bensanMaara > 0)
+		{
+			bensanMaara = bensanMaara - 1;
+		}
+		else 
+		{
+			System.out.println("Bensa lopussa, et voi kiihdyttää");
+		}
 	}	
 	public void naytaTiedot()
 	{
 		System.out.println(merkki);
 		System.out.println(malli);
 		System.out.println(bensanMaara);
+	}
+	public void tankkaaAuto(int tankkaa)
+	{
+		System.out.println("Tankissa bensaa " + bensanMaara);
+		System.out.println("Tankkaus: " + tankkaa);
+		bensanMaara = bensanMaara + tankkaa;
+		System.out.println("Tankissa bensaa tankkauksen jälkeen " + bensanMaara);
 	}
 }
 	
