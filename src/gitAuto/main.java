ccea0a8d6d376bb4acbc373da1ead7c4fac9a9f6
elias1235813 <elias.vuorenmaa706@gmail.com>
2022-12-05 09:01:16 +0200

    lisätty auton attribuutit, kolme metodia ja ensimmäinen olio

--- src/gitAuto/main.java
+++ src/gitAuto/main.java
@@ -3,8 +3,39 @@ package gitAuto;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// muodostetaan auto-olio
+		Auto auto1 = new Auto();
+		auto1.merkki = "Toyota";
+		auto1.malli = "Avensis";
+		auto1.bensanMaara = 10;
+		auto1.naytaTiedot();
+		auto1.kiihdyta();
+		auto1.naytaTiedot();
 
 	}
 
+} // end of main
+class Auto{
+	// ominaisuudet
+	String merkki;
+	String malli;
+	int bensanMaara = 0;
+	
+	//toiminnot
+	public void jarruta()
+	{
+		System.out.println("Auto jarruttaa");
+	}
+	public void kiihdyta()
+	{
+		System.out.println("Auto kiihdyttaa");
+		bensanMaara = bensanMaara - 1;
+	}	
+	public void naytaTiedot()
+	{
+		System.out.println(merkki);
+		System.out.println(malli);
+		System.out.println(bensanMaara);
+	}
 }
+	
